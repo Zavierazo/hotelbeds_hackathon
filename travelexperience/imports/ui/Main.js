aed1708d77839620e94f5d40c2004c52f81d4a35
lexfaraday <lexfaraday@gmail.com>
2018-02-23 03:34:48 +0100

    Stars size

--- travelexperience/imports/ui/Main.js
+++ travelexperience/imports/ui/Main.js
@@ -228,7 +228,7 @@ class Main extends Component {
                       <div className="content">
                         <a href="#" className="title h5">Surfing the Atlantic</a>
                         <div className="rating_photo" >
-                          <img src="img/5stars.jpg" alt="photo" />                          
+                          <img src="img/5stars.png" alt="photo" />                          
                         </div>
                     
                         <div className="swiper-container">
